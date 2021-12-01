fb4c47025100451a1eb1f4f55304c8b19f53f50d
Mariia Bukiaeva <bukaieva.mariia@kiu.edu.ge>
2021-12-01 19:06:55 +0400

    no message

---
+++ src/pgdp/collections/LinkedQueue.java
@@ -0,0 +1,13 @@
+package pgdp.collections;
+
+public abstract class LinkedQueue implements Queue{
+    @Override
+    public void enqueue() {
+
+    }
+
+    @Override
+    public Object dequeue() {
+        return null;
+    }
+}
---
+++ src/pgdp/collections/LinkedStack.java
@@ -0,0 +1,14 @@
+package pgdp.collections;
+
+public abstract class LinkedStack implements Stack {
+
+    @Override
+    public void push() {
+
+    }
+
+    @Override
+    public Object pop() {
+        return null;
+    }
+}
