4220763785233b90bf1ad600a7d551b1ffc73355
Mariia Bukiaeva <bukaieva.mariia@kiu.edu.ge>
2021-12-02 19:59:22 +0400

    no message

--- src/pgdp/collections/LinkedQueue.java
+++ src/pgdp/collections/LinkedQueue.java
@@ -1,23 +1,53 @@
 package pgdp.collections;
 
 public class LinkedQueue<T> implements Queue<T> {
+    private List<T> current;
+    private int count;
+
     @Override
     public int size() {
-        return 0;
+        return count;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size() == 0;
     }
 
     @Override
     public void enqueue(T obj) {
+        if(isEmpty()) current = new List<>(obj,null);
+        else{
+            List<T> temp = current;
+            while(temp.getNext() != null){
+                temp = temp.getNext();
+            }
+            temp.insert(obj);
+        }
+        count++;
+
+
 
     }
 
+
+    /*List<T> newList = new List<T>(obj);
+        if (this.first == null) {
+            this.first = this.last = newList;
+        }
+        List<T> beforeFirst = this.first.getNext();
+        beforeFirst = newList;
+        this.first = newList;*/
+
+
     @Override
     public T dequeue() {
-        return null;
+        // T result = first.getInfo();
+       // if(last == first) last = null;
+        if(current == null)return null;
+        T result = current.getInfo();
+        current = current.getNext();
+        count--;
+        return result;
     }
 }
