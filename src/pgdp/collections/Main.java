d544ccefab2ca852fcbcccb028a586b0c1f98b67
Mariia Bukiaeva <bukaieva.mariia@kiu.edu.ge>
2021-12-04 19:00:20 +0400

    no message

---
+++ src/pgdp/collections/Main.java
@@ -0,0 +1,22 @@
+package pgdp.collections;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+        /*LinkedQueue<String> st= new LinkedQueue<>() ;
+        st.enqueue("ABC");
+        st.enqueue("DEF");
+        st.enqueue("G");
+        st.enqueue("HYI");
+        st.dequeue();
+        st.dequeue();*/
+
+    }
+
+}
