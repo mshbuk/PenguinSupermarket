f77232d155975c4b7c9bf64f43839ce60da01936
Mariia Bukiaeva <bukaieva.mariia@kiu.edu.ge>
2021-12-04 17:44:49 +0400

    no message

--- src/pgdp/collections/DataStructureLink.java
+++ src/pgdp/collections/DataStructureLink.java
@@ -5,22 +5,22 @@ public class DataStructureLink<T> {
     private DataStructureConnector<T> B;
 
     public DataStructureLink(DataStructureConnector<T> A,DataStructureConnector<T> B) {
-        this.A = A;
-        this.B = B;
+       // this.A = A;
+      //  this.B = B;
     }
 
     public boolean moveNextFromAToB() {
-        if(!A.hasNextElement()) return false;
-        else {
-            T obj = A.removeNextElement();
-            B.addElement(obj);
-            return true;
-        }
-    }
+      //  if(!A.hasNextElement()) return false;
+    //    else {
+     //       T obj = A.removeNextElement();
+      //      B.addElement(obj);
+     //       return true;
+    //    }
+        return false;}
     public void moveAllFromAToB() {
-    while(!(A.hasNextElement())) {
-        T obj = A.removeNextElement();
-        B.addElement(obj);
-    }
+   // while(!(A.hasNextElement())) {
+     //   T obj = A.removeNextElement();
+     //   B.addElement(obj);
+   // }
     }
 }
