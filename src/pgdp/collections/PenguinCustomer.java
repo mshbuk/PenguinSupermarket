ae9ca5a1a287006cfa0c0766a6188218220f3625
Mariia Bukiaeva <bukaieva.mariia@kiu.edu.ge>
2021-12-04 19:44:27 +0400

    no message

--- src/pgdp/collections/PenguinCustomer.java
+++ src/pgdp/collections/PenguinCustomer.java
@@ -1,9 +1,9 @@
 package pgdp.collections;
 
 public class PenguinCustomer {
-/*    private String name;
+   private String name;
     private int availableMoney;
-    private final Stack<FishyProduct> productsInTheCart;
+    private Stack<FishyProduct> productsInTheCart;
 
     public String getName() {
         return name;
@@ -17,7 +17,7 @@ public class PenguinCustomer {
         return productsInTheCart;
     }
 
-    public PenguinCustomer(final String name, final int availableMoney, Stack<FishyProduct> productsInTheCart) {
+    public PenguinCustomer(final String name, int availableMoney, Stack<FishyProduct> productsInTheCart) {
         if(name != null) this.name = name;
         if(availableMoney > 0) this.availableMoney = availableMoney;
         this.productsInTheCart = productsInTheCart;
@@ -46,5 +46,5 @@ public class PenguinCustomer {
     @Override
     public String toString() {
         return "PenguinCustomer " + name + ", " + availableMoney + ", " + productsInTheCart;
-    }*/
+    }
 }
