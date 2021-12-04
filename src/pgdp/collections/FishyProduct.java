2b6d645e58684efd3785e7f2b0a9e82b06c53a94
Mariia Bukiaeva <bukaieva.mariia@kiu.edu.ge>
2021-12-04 18:52:00 +0400

    no message

---
+++ src/pgdp/collections/FishyProduct.java
@@ -0,0 +1,29 @@
+package pgdp.collections;
+
+public class FishyProduct {
+    private String name;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public FishyProduct(final String name, final int price) {
+        if(name != null) this.name = name;
+        if(price > 0) this.price = price;
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("new FishyProduct (");
+        builder.append(getName()).append(", ");
+        builder.append(getPrice()).append(")");
+        return super.toString();
+    }
+}
