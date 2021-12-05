6fe383d68ede7393030ac5963bc6d24228f513c1
Mariia Bukiaeva <bukaieva.mariia@kiu.edu.ge>
2021-12-05 16:52:33 +0400

    no message

--- src/pgdp/collections/PenguinCustomer.java
+++ src/pgdp/collections/PenguinCustomer.java
@@ -1,53 +1,56 @@
 package pgdp.collections;
 
 public class PenguinCustomer {
-   private String name;
-    private int availableMoney;
-    private Stack<FishyProduct> productsInTheCart;
+    private String name;
+    private int money;
+    private Stack<FishyProduct> products;
+
 
     public String getName() {
         return name;
     }
 
-    public int getAvailableMoney() {
-        return availableMoney;
+    public int getMoney() {
+        return money;
     }
 
-    public Stack<FishyProduct> getStack() {
-        return productsInTheCart;
+    public Stack<FishyProduct> getProducts() {
+        return products;
     }
 
     public PenguinCustomer(final String name, int availableMoney, Stack<FishyProduct> productsInTheCart) {
-        if(name != null) this.name = name;
-        if(availableMoney > 0) this.availableMoney = availableMoney;
-        this.productsInTheCart = productsInTheCart;
+        if (name != null) this.name = name;
+        if (availableMoney > 0) this.money = availableMoney;
+        this.products = productsInTheCart;
     }
 
     public void addProductToBasket(FishyProduct item) {
-        productsInTheCart.push(item);
+        products.push(item);
     }
 
     public void placeAllProductsOnBand(Queue<FishyProduct> band) {
-    while(!(productsInTheCart.isEmpty())) {
-        FishyProduct item = productsInTheCart.pop();
-         band.enqueue(item);
-    }
+        DataStructureLink<FishyProduct> newBand;
+        //while(!(productsInTheCart.isEmpty())) {
+        //   FishyProduct item = productsInTheCart.pop();
+        //   band.enqueue(item);
+        //}
     }
 
     public void takeAllProductsFromBand(Queue<FishyProduct> band) {
-        while(!band.isEmpty()) {
-            FishyProduct item = band.dequeue();
-            productsInTheCart.push(item);
-        }
+        //  while(!band.isEmpty()) {
+        //     FishyProduct item = band.dequeue();
+       // productsInTheCart.push(item);
     }
 
+
+
     public void pay(int price) {
-        if(price > 0 && availableMoney > 0)
-    availableMoney = availableMoney - price;
+        if (price > 0 && money > 0)
+            money = money - price;
     }
 
     @Override
     public String toString() {
-        return "PenguinCustomer " + name + ", " + availableMoney + ", " + productsInTheCart;
+        return "PenguinCustomer " + name + ", " + money + ", " + products;
     }
 }
