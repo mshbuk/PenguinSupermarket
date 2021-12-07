5923da10efdf996a4a3ec70ff6d8dea1f0b53cf8
Mariia Bukiaeva <bukaieva.mariia@kiu.edu.ge>
2021-12-07 22:30:22 +0400

    no message

--- src/pgdp/collections/PenguinSupermarket.java
+++ src/pgdp/collections/PenguinSupermarket.java
@@ -1,7 +1,7 @@
 package pgdp.collections;
 
 public class PenguinSupermarket {
-    private final Checkout[] checkouts;
+    private Checkout[] checkouts;
 
     public Checkout[] getCheckouts() {
         return checkouts;
@@ -41,15 +41,35 @@ public class PenguinSupermarket {
         Checkout closedRegister = checkouts[index];
         int newLengthOfRegisters = checkouts.length - 1;
         Checkout[] openRegisters = new Checkout[newLengthOfRegisters];
-        for(int i = 0; i < checkouts.length; i++) {
-            if(i < index) {
+
+        if (checkouts.length == 0 || checkouts.length == 1) {
+            ExceptionUtil.illegalArgument("Not allowed");
+
+        } else if (index < 0 || index >= checkouts.length) {
+            ExceptionUtil.illegalArgument("Not allowed");
+        }
+
+        for (int i = 0; i < checkouts.length; i++) {
+            if (i < index) {
                 openRegisters[i] = checkouts[i];
-            }
-            else if(i > index) {
+            } else if (i > index) {
                 openRegisters[i - 1] = checkouts[i];
             }
         }
+        checkouts = openRegisters;
+        if (!closedRegister.getQueue().isEmpty()) {
+            DataStructureConnector<PenguinCustomer> getPenguinsOut = new QueueConnector<>(closedRegister.getQueue());
+            DataStructureConnector<PenguinCustomer> reverseOrder = new StackConnector<>(new LinkedStack<>());
+            DataStructureLink<PenguinCustomer> movePenguins = new DataStructureLink<>(getPenguinsOut, reverseOrder);
 
+            while (getPenguinsOut.hasNextElement()) {
+                movePenguins.moveNextFromAToB();
+            }
+
+            while (reverseOrder.hasNextElement()) {
+                reverseOrder.removeNextElement().goToCheckout(this);
+            }
+        }
     }
 
     public void serveCustomers() {
