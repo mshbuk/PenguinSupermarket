b3903255d31592771925d9e358e168320806832e
Mariia Bukiaeva <bukaieva.mariia@kiu.edu.ge>
2021-12-07 12:56:48 +0400

    no message

---
+++ src/pgdp/collections/Checkout.java
@@ -0,0 +1,55 @@
+package pgdp.collections;
+
+public class Checkout {
+    private final Queue<PenguinCustomer> queue;
+    private final Queue<FishyProduct> bandBeforeCashier;
+    private final Queue<FishyProduct> bandAfterCashier;
+
+
+    public Checkout() {
+        this.queue = new LinkedQueue<>();
+        this.bandBeforeCashier = new LinkedQueue<>();
+        this.bandAfterCashier = new LinkedQueue<>();
+    }
+
+    public Queue<PenguinCustomer> getQueue() {
+        return queue;
+    }
+
+    public Queue<FishyProduct> getBandBeforeCashier() {
+        return bandBeforeCashier;
+    }
+
+    public Queue<FishyProduct> getBandAfterCashier() {
+        return bandAfterCashier;
+    }
+
+    public int queueLength() {
+        return queue.size();
+    }
+
+    public void serveNextCustomer(){
+    if(!queue.isEmpty()) {
+       PenguinCustomer larisa = queue.dequeue();
+       larisa.placeAllProductsOnBand(bandBeforeCashier);
+
+       int receipt = 0;
+        while(!(bandBeforeCashier.size() == 0)) {
+           FishyProduct item = bandBeforeCashier.dequeue();
+           int priceOfTheItem = receipt + item.getPrice();
+           larisa.pay(priceOfTheItem);
+           bandAfterCashier.enqueue(item);;
+
+           if(larisa.getMoney() < receipt) {
+               ExceptionUtil.illegalArgument("Not allowed");
+           }
+       }
+       larisa.takeAllProductsFromBand(bandAfterCashier);
+    }
+
+
+
+
+
+    }
+}
